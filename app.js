886a62423ef75ebdc7e97b2c7e8b7b3eec74a1ba
Onoriode Dafetta <onoriodedafetta@gmail.com>
2023-02-08 15:07:03 +0100

    changed images and solutions contents

---
+++ app.js
@@ -0,0 +1,25 @@
+let modal = document.getElementById("myModal");
+let nav = document.getElementById("nav");
+let images = document.getElementsByClassName("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+
+for (let i = 0; i < images.length; i++) {
+   let img = images[i];
+
+   img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    nav.style.display = "none";
+   }
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none"
+  nav.style.display = "block"
+}
